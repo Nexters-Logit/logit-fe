8229309ad41dacda78b489d3f0b33aade7313021
ljh0608 <m98m@naver.com>
2026-01-31 07:06:24 +0900

    feat: 현재 질문 ui 추가

--- app/(main)/chat/[questionId]/_components/ChatArea/ChatAreaClient.tsx
+++ app/(main)/chat/[questionId]/_components/ChatArea/ChatAreaClient.tsx
@@ -138,6 +138,7 @@ export function ChatAreaClient({
           onQuestionChange={handleQuestionChange}
         />
       </div>
+      <h1 className="text-title-3 text-gray-400">{currentQuestion.question}</h1>
 
       {/* 채팅 메시지 영역 */}
       <div className="flex-1 min-h-0 overflow-hidden">
